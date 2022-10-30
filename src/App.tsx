6a0645262ad90ccfe76982046c41ac5753b437dc
Ishmael Kargbo <ishmaelkargbo5@outlook.com>
2022-10-30 15:47:33 +0000

    design task
    1. task title
    2. task header
    3. task list and rows

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,20 @@
-import React from 'react';
-
 function App() {
   return (
     <div>
       <main className='main-container'>
-      <h1 className='title'>My Day</h1>
-      <section>
-        <div>
-          <h4>TDOD</h4>
-          <button>Plus</button>
-        </div>
-      </section>
+        <h1 className='title'>My Day</h1>
+        <section className='task-container'>
+          <h4 className='title todo'>TODO</h4>
+          <div className='header'>
+            <button className='add-btn'>+</button>
+          </div>
+          <ul className="list">
+            <li className="row">
+              <input id="task1" type="checkbox" />
+              <label htmlFor="task1" className="task">Eat today by five</label>
+            </li>
+          </ul>
+        </section>
       </main>
     </div>
   );
